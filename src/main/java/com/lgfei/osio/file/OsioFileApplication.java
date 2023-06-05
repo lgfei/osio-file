9937dd10ec779a36d9337703a021653364654d53
long.gf <long.gf@topsports.com.cn>
2023-06-05 12:24:37 +0800

    init by https://start.aliyun.com/

---
+++ src/main/java/com/lgfei/osio/file/OsioFileApplication.java
@@ -0,0 +1,13 @@
+package com.lgfei.osio.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsioFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OsioFileApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lgfei/osio/file/OsioFileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgfei.osio.file;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsioFileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
